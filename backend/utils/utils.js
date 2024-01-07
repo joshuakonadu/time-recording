068d68659008827b0fe28ed0785932f378aec094
joshua <j.konadu@tomorrowlabs.io>
2024-01-07 16:32:35 +0100

    clean up workspace message when leaving workspace

--- backend/utils/utils.js
+++ backend/utils/utils.js
@@ -1,12 +1,14 @@
 import { unregisterWorkspace } from "./registerWorkspace.helper.js";
 import { workspaceRemoveMember } from "./workspace.helper.js";
 import { deleteAllTimeRecordsByUser } from "./timerecord.helper.js";
+import { removeWorkspaceInvitation } from "./invitation.helper.js";
 
 export const deleteUserWorkspaceData = async ({ workspaceId, userId }) => {
   const actions = [
     unregisterWorkspace,
     workspaceRemoveMember,
     deleteAllTimeRecordsByUser,
+    removeWorkspaceInvitation,
   ];
   const promises = actions.map((action) => action(userId, workspaceId));
   return Promise.all(promises);
